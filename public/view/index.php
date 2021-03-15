f7b116f06e4bb02a67e57c244f40b6986f17def5
antar <toobrokeforrysll@gmail.com>
2021-03-15 15:19:39 +0100

    feat: config changes

--- public/view/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-// index.php
-
-    require_once '../controller/config.php';
-    require_once '../controller/database.php';
-    
-    $db = new database($pdo);
-    $rows = $db->getData();
-?>
