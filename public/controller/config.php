5c10d9a1a388d9eb3232955329ee17822bd72421
antar <toobrokeforrysll@gmail.com>
2021-03-15 14:50:06 +0100

    feat: added database connection

---
+++ public/controller/config.php
@@ -0,0 +1,16 @@
+<?php
+
+    $host = '127.0.0.1';
+    $db_name = 'm133';
+    $db_username = 'm133';
+    $db_password = 'm133';
+
+    try
+    {
+        $pdo = new PDO('mysql:host='. $host .';dbname='.$db_name, $db_username, $db_password);
+    }
+    catch (PDOException $e)
+    {
+        exit('Error Connecting To DataBase');
+    }
+?>
---
+++ public/controller/database.php
@@ -0,0 +1,18 @@
+<?php 
+// database.php
+
+class database
+{
+    function __construct($pdo)
+    {
+        $this->pdo = $pdo;
+    }
+
+    function getData()
+    {
+        $query = $this->pdo->prepare('SELECT * FROM database');
+        $query->execute();
+        return $query->fetchAll();
+    }
+}
+?>
