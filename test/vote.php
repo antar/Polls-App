bd4c516fa5b0278a5e45056c8c5aa13b7d01f72d
antar <toobrokeforrysll@gmail.com>
2021-03-16 10:53:27 +0100

    feat: result GET request that will get the poll ID

--- test/vote.php
+++ test/vote.php
@@ -5,14 +5,19 @@ include 'functions.php';
 $pdo = pdo_connect_mysql();
 
 if (isset($_GET['id'])) {
+
     $stmt = $pdo->prepare('SELECT * FROM polls WHERE id = ?');
     $stmt->execute([$_GET['id']]);
+
     $poll = $stmt->fetch(PDO::FETCH_ASSOC);
 
     if ($poll) {
+
         $stmt = $pdo->prepare('SELECT * FROM poll_answers WHERE poll_id = ?');
         $stmt->execute([$_GET['id']]);
+
         $poll_answers = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        
         if (isset($_POST['poll_answer'])) {
             $stmt = $pdo->prepare('UPDATE poll_answers SET votes = votes + 1 WHERE id = ?');
             $stmt->execute([$_POST['poll_answer']]);
