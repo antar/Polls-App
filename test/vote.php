fea6de003fc50e195f803e69c271b5cc38808c3c
antar <toobrokeforrysll@gmail.com>
2021-03-16 10:48:55 +0100

    feat: iterate each answer and create the input radio fields

--- test/vote.php
+++ test/vote.php
@@ -26,3 +26,24 @@ if (isset($_GET['id'])) {
     die ('No poll ID specified.');
 }
 ?>
+
+<?=template_header('Poll Vote')?>
+
+<div class="content poll-vote">
+	<h2><?=$poll['title']?></h2>
+	<p><?=$poll['desc']?></p>
+    <form action="vote.php?id=<?=$_GET['id']?>" method="post">
+        <?php for ($i = 0; $i < count($poll_answers); $i++): ?>
+        <label>
+            <input type="radio" name="poll_answer" value="<?=$poll_answers[$i]['id']?>"<?=$i == 0 ? ' checked' : ''?>>
+            <?=$poll_answers[$i]['title']?>
+        </label>
+        <?php endfor; ?>
+        <div>
+            <input type="submit" value="Vote">
+            <a href="result.php?id=<?=$poll['id']?>">View Result</a>
+        </div>
+    </form>
+</div>
+
+<?=template_footer()?>
