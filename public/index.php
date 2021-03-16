189b9b6b3080c643f9bd85e85f294d4d5fa56704
antar <toobrokeforrysll@gmail.com>
2021-03-16 11:26:42 +0100

    fix: code styling

--- public/index.php
+++ public/index.php
@@ -1,9 +1,14 @@
 <?php
+
 include 'functions.php';
+
 $pdo = pdo_connect_mysql();
+
 $stmt = $pdo->query('SELECT p.*, GROUP_CONCAT(pa.title ORDER BY pa.id) AS answers FROM polls p LEFT JOIN poll_answers pa ON pa.poll_id = p.id GROUP BY p.id');
 $polls = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
 ?>
+
 <?=template_header('Polls')?>
 
 <div class="content home">
