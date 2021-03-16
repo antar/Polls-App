6a6c3a2f24b74bd0f1aaa8931c1fe18c98b57708
antar <toobrokeforrysll@gmail.com>
2021-03-16 12:41:48 +0100

    removed datanase function in file

--- public/functions.php
+++ public/functions.php
@@ -1,22 +1,5 @@
 <?php
 
-  function pdo_connect_mysql() {
-      $DATABASE_HOST = '127.0.0.1';
-      $DATABASE_USER = 'm133';
-      $DATABASE_PASS = 'm133';
-      $DATABASE_NAME = 'phppoll';
-
-      try {
-
-        return new PDO('mysql:host=' . $DATABASE_HOST . ';dbname=' . $DATABASE_NAME . ';charset=utf8', $DATABASE_USER, $DATABASE_PASS);
-
-      } catch (PDOException $exception) {
-
-        die ('Failed to connect to database!');
-
-      }
-  }
-
   function template_header($title) {
     echo <<<EOT
     <!DOCTYPE html>
